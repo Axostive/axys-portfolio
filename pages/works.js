6cd1aab3fbbdd00f9fe03606e08b2063df15384b
Rodolfo Spier <rodolfo.spier@gmail.com>
2022-10-16 16:46:16 -0300

    Commit to deploy

---
+++ pages/works.js
@@ -0,0 +1,17 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Section } from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
+
+const Works = () => (
+  <Layout>
+    <Container mt={20}>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works - IN PROGRESS!
+      </Heading>
+    </Container>
+  </Layout>
+)
+
+export default Works
+export { getServerSideProps } from '../components/chakra'
